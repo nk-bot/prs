c6ff2a55f764c8f4536a3c4b7a3b44351978a204
Kapadia <nainish4352@gmail.com>
2025-11-04 14:46:14 +0530

    bulk112

--- scrapers/fc.js
+++ scrapers/fc.js
@@ -1,7 +1,6 @@
 import chromium from "@sparticuz/chromium";
-import puppeteer from "puppeteer-core";
+import puppeteer from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
-
 puppeteer.use(StealthPlugin());
 
 export async function scrapeFirstCry(url) {
